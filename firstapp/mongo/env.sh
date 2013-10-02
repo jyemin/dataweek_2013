b1f547504ae2d468713d7951f88c8569142aa673
Sridhar Nanjundeswaran <sridhar@10gen.com>
2013-10-02 09:20:44 -0700

    minor edits

--- firstapp/mongo/env.sh
+++ firstapp/mongo/env.sh
@@ -4,4 +4,4 @@ datadir="$githome/firstapp/data"
 if [ ! -d "$datadir" ]; then
     mkdir -p "$datadir"
 fi
-export PATH=$PATH:/opt/mongodb-osx-x86_64-2.2.2/bin
+export PATH=$PATH:/opt/mongodb-osx-x86_64-2.4.3/bin
